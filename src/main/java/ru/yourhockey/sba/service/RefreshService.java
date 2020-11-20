e826102857ef8b15f4c0a68007a4287bf5f53ad8
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-11-20 22:01:17 +0300

    develop: start implementation of dump

--- src/main/java/ru/yourhockey/sba/service/RefreshService.java
+++ src/main/java/ru/yourhockey/sba/service/RefreshService.java
@@ -1,6 +1,7 @@
 package ru.yourhockey.sba.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yourhockey.sba.config.MatcherOfferConfig;
 
@@ -9,6 +10,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RefreshService {
@@ -16,14 +18,19 @@ public class RefreshService {
     private final MatcherOfferConfig matcherOfferConfig;
 
     public void refreshMatcherOffers() {
+        log.info("Start refreshMatcherOffers()");
         File directory = new File(matcherOfferConfig.getFilePath());
+        log.info("Get directory: {}", directory);
         File[] files = directory.listFiles();
+        log.info("Get list of files: {}", files);
         Arrays.sort(files, Comparator.comparingLong(File::lastModified));
         File file = files[0];
 
+        log.info("Start of cmd processing");
         ProcessBuilder processBuilder = new ProcessBuilder(
+                "sudo",
+                "-u " + matcherOfferConfig.getUsername(),
                 "psql",
-                "-U " + matcherOfferConfig.getUsername(),
                 "matchingservice",
                 "-f " + file.getAbsolutePath()
         );
@@ -32,9 +39,12 @@ public class RefreshService {
         try {
             start = processBuilder.start();
             start.waitFor();
+            log.info("Finishing of cmd processing");
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            log.error("Failing executing of command");
+            log.error(e.getMessage());
         }
+        log.info("Successful executing of command");
 
     }
 
