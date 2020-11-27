411d4c0cb4260f750949bbe075c11fafff1e7396
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-11-27 13:06:44 +0300

    main:enable scheduling

--- src/main/java/ru/yourhockey/sba/SbaApplication.java
+++ src/main/java/ru/yourhockey/sba/SbaApplication.java
@@ -4,9 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.yourhockey.sba.config.DumpConfig;
 
 
+@EnableScheduling
 @EnableAdminServer
 @SpringBootApplication
 @EnableConfigurationProperties({DumpConfig.class})
